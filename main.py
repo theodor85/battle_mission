6af853f5ea67c1bfcc898f42b21c37950a0611f0
Teddy Coder <fedor_coder@mail.ru>
2026-03-13 19:43:44 +0100

    moving on map

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, random, time
+import sys, random
 
 import pygame
 from pygame.locals import *
@@ -11,21 +11,73 @@ SCREEN_HEIGHT = 1000
 
 FPS = 60
 FramePerSec = pygame.time.Clock()
- 
-# Predefined some colors
+
+# Colors
 BLUE  = (0, 0, 255)
 RED   = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+# Map
+TILE_SIZE = 50
+MAP_COLS = 50
+MAP_ROWS = 50
+WORLD_WIDTH = MAP_COLS * TILE_SIZE
+WORLD_HEIGHT = MAP_ROWS * TILE_SIZE
+
+# Tile types
+GROUND = 0
+ROCK = 1
+WATER = 2
+
+TILE_COLORS = {
+    GROUND: (34, 139, 34),
+    ROCK:   (128, 128, 128),
+    WATER:  (0, 105, 148),
+}
+
+# Physics
+MASS = 15.0
+MOVING_POWER = 10.0
+DAMPING = 0.85
+
+# Camera
+LOOK_AHEAD_FACTOR = 15.0
+SMOOTH_FACTOR = 0.08
+
 DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Moving")
 
-MASS = 15.0
-MOVING_POWER = 10.0  # 5.0 - хорошо
-DRAG = 0.4          # 0.3 - хорошо
+
+def generate_map():
+    tile_map = [[GROUND] * MAP_COLS for _ in range(MAP_ROWS)]
+    for r in range(MAP_ROWS):
+        for c in range(MAP_COLS):
+            val = random.random()
+            if val < 0.10:
+                tile_map[r][c] = ROCK
+            elif val < 0.18:
+                tile_map[r][c] = WATER
+    return tile_map
+
+
+def draw_tiles(surface, tile_map, camera):
+    start_col = max(0, int(camera.x // TILE_SIZE))
+    start_row = max(0, int(camera.y // TILE_SIZE))
+    end_col = min(MAP_COLS, int((camera.x + SCREEN_WIDTH) // TILE_SIZE) + 1)
+    end_row = min(MAP_ROWS, int((camera.y + SCREEN_HEIGHT) // TILE_SIZE) + 1)
+
+    for r in range(start_row, end_row):
+        for c in range(start_col, end_col):
+            world_x = c * TILE_SIZE
+            world_y = r * TILE_SIZE
+            screen_x, screen_y = camera.apply(world_x, world_y)
+            pygame.draw.rect(
+                surface,
+                TILE_COLORS[tile_map[r][c]],
+                (screen_x, screen_y, TILE_SIZE, TILE_SIZE),
+            )
 
 
 class Player(pygame.sprite.Sprite):
@@ -35,6 +87,8 @@ class Player(pygame.sprite.Sprite):
         self.surf.fill(BLUE)
         self.rect = self.surf.get_rect()
 
+        self.x = float(WORLD_WIDTH // 2)
+        self.y = float(WORLD_HEIGHT // 2)
         self.speed_x = 0
         self.speed_y = 0
         self.moving_power_x = 0
@@ -43,8 +97,9 @@ class Player(pygame.sprite.Sprite):
     def move(self):
         self.handle_input()
         self.calculate_speed()
-        self.rect.move_ip(int(self.speed_x), int(self.speed_y))
-        self.keep_on_screen()
+        self.x += self.speed_x
+        self.y += self.speed_y
+        self.keep_on_map()
 
     def handle_input(self):
         self.moving_power_x = 0
@@ -62,50 +117,54 @@ class Player(pygame.sprite.Sprite):
             self.moving_power_x = MOVING_POWER
 
     def calculate_speed(self):
-        resulting_power_x = self.moving_power_x - self.resisting_power_x()
-        resulting_power_y = self.moving_power_y - self.resisting_power_y()
+        self.speed_x = (self.speed_x + self.moving_power_x / MASS) * DAMPING
+        self.speed_y = (self.speed_y + self.moving_power_y / MASS) * DAMPING
 
-        self.speed_x = self.speed_x + ((resulting_power_x / MASS))
-        self.speed_y = self.speed_y + ((resulting_power_y / MASS))
-
-        print("------------------------------------------------------")
-        print(f"Moving Power X: {self.moving_power_x}, Moving Power Y: {self.moving_power_y}")
-        print(f"Resisting Power X: {self.resisting_power_x()}, Resisting Power Y: {self.resisting_power_y()}")
-        print(f"Speed X: {self.speed_x}, Speed Y: {self.speed_y}")
-    
-    def resisting_power_x(self):
-        if self.speed_x < 0:
-            return -DRAG * self.speed_x * self.speed_x
-        else:
-            return DRAG * self.speed_x * self.speed_x
-
-    def resisting_power_y(self):
-        if self.speed_y < 0:
-            return -DRAG * self.speed_y * self.speed_y
-        else:
-            return DRAG * self.speed_y * self.speed_y
-
-    def keep_on_screen(self):
-        if self.rect.left < 0:
-            self.rect.left = 0
+        if abs(self.speed_x) < 0.01:
             self.speed_x = 0
+        if abs(self.speed_y) < 0.01:
+            self.speed_y = 0
+
+    def keep_on_map(self):
+        if self.x < 0:
+            self.x = 0
             if self.speed_x < 0:
                 self.speed_x = 0
-        if self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
-            self.speed_x = 0
+        if self.x + self.rect.width > WORLD_WIDTH:
+            self.x = WORLD_WIDTH - self.rect.width
             if self.speed_x > 0:
                 self.speed_x = 0
-        if self.rect.top <= 0:
-            self.rect.top = 0
+        if self.y < 0:
+            self.y = 0
             if self.speed_y < 0:
                 self.speed_y = 0
-        if self.rect.bottom >= SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
+        if self.y + self.rect.height > WORLD_HEIGHT:
+            self.y = WORLD_HEIGHT - self.rect.height
             if self.speed_y > 0:
                 self.speed_y = 0
 
 
+class Camera:
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
+
+    def update(self, player):
+        target_x = (player.x + player.speed_x * LOOK_AHEAD_FACTOR) - SCREEN_WIDTH / 2
+        target_y = (player.y + player.speed_y * LOOK_AHEAD_FACTOR) - SCREEN_HEIGHT / 2
+
+        self.x += (target_x - self.x) * SMOOTH_FACTOR
+        self.y += (target_y - self.y) * SMOOTH_FACTOR
+
+        self.x = max(0, min(self.x, WORLD_WIDTH - SCREEN_WIDTH))
+        self.y = max(0, min(self.y, WORLD_HEIGHT - SCREEN_HEIGHT))
+
+    def apply(self, world_x, world_y):
+        return int(world_x - self.x), int(world_y - self.y)
+
+
+tile_map = generate_map()
+camera = Camera()
 P1 = Player()
 
 while True:
@@ -115,10 +174,11 @@ while True:
             sys.exit()
 
     P1.move()
+    camera.update(P1)
 
-    DISPLAYSURF.fill(WHITE)
-    DISPLAYSURF.blit(P1.surf, P1.rect)
+    DISPLAYSURF.fill(BLACK)
+    draw_tiles(DISPLAYSURF, tile_map, camera)
+    DISPLAYSURF.blit(P1.surf, camera.apply(P1.x, P1.y))
 
     pygame.display.update()
     FramePerSec.tick(FPS)
-
